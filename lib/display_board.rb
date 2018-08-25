fc6172c7f7e81c5bac1ffc8d388c142d23c6fd24
Stephanie Wilson <stephanie7423@gmail.com>
2018-08-25 20:22:04 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,11 @@
 #board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 
 def display_board(board)
-  print " #{board[0]} | #{board[1]} | #{board[2]} "+/n+
-  print "-----------"+/n+
-  print " #{board[3]} | #{board[4]} | #{board[5]} "+/n+
-  print "-----------"+/n+
-  print " #{board[6]} | #{board[7]} | #{board[8]} "+/n+
+  puts "#{board[0]} | #{board[1]} | #{board[2]} "+/n+
+  puts "-----------"+/n+
+  puts "#{board[3]} | #{board[4]} | #{board[5]} "+/n+
+  puts "-----------"+/n+
+  puts "#{board[6]} | #{board[7]} | #{board[8]} "+/n+
 end
 
 #display_board(board)
