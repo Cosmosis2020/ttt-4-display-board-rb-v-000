9a5bb14c128772cbd36dfc1f383ffbaf7a3ca60b
Stephanie Wilson <stephanie7423@gmail.com>
2018-08-25 19:53:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 def display_board(board)
   print " #{board[0]} | #{board[1]} | #{board[2]} "+/n+
-  print "-----------"
+  print "-----------"+/n+
   print " #{board[3]} | #{board[4]} | #{board[5]} "+/n+
-  print "-----------"
+  print "-----------"+/n+
   print " #{board[6]} | #{board[7]} | #{board[8]} "+/n+
 end
 
