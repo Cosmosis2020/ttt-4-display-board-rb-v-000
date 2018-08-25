ffdacb7ac1ea966278a445663b4dc8f58e578a64
Stephanie Wilson <stephanie7423@gmail.com>
2018-08-25 20:33:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,10 +4,10 @@
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "+/n+
-  puts"-----------"+/n+
-  puts" #{board[3]} | #{board[4]} | #{board[5]} "+/n+
-  puts"-----------"+/n+
-  puts" #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "-----------"+/n+
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "+/n+
+  puts "-----------"+/n+
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 #display_board(board)
